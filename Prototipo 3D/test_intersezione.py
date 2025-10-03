704effaba86751575094c6cf1e4588815fe73d31
AtomicDonuts <Pascal98@hotmail.it>
2025-10-03 22:25:22 +0200

    test_intersezione

--- Prototipo 3D/test_intersezione.py
+++ Prototipo 3D/test_intersezione.py
@@ -6,12 +6,13 @@ import numpy as np
 
 linea = lib.Muon()
 linea.originpos = lib.Point3D(0, 0, 0)
-linea.phi = np.pi/2
-linea.theta = 0
+linea.theta = np.pi / 2
+linea.phi = 0
 linea.d_vector = linea._direction_vector()
 print(linea.d_vector)
 
 para = lib.Paralleogram()
 para.set_position((1, -0.5, -0.5))
 para.set_dimensions(1, 1, 1)
-para.intersect_with_line(linea)
+if para.intersect_with_line(linea)[0]:
+    print(f"{para.intersect_with_line(linea)[1][0]}\n{para.intersect_with_line(linea)[1][1]}")
