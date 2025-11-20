9a988e18d370b3b25ede9a4113d719e56dc5967e
AtomicDonuts <Pascal98@hotmail.it>
2025-11-20 18:52:28 +0100

    shortcuts

---
+++ imports/custom_variables.py
@@ -0,0 +1,33 @@
+'''
+Moduletto Python per la definizione di alcune variabili globali.
+-----------------------
+Deve essere importato nei vari script utilizzando la seguente sintassi:
+import sys
+sys.path.append("../imports/")
+from custom_variables import *
+-----------------------
+'''
+from pathlib import Path
+
+# Folders
+if Path("../imports").exists():
+    dir_imports_path = Path("../imports")
+else:
+    raise FileNotFoundError("'../imports' not found.")
+if Path("../map").exists():
+    dir_map_path = Path("../map")
+else:
+    raise FileNotFoundError("'../map' not found.")
+if Path("../xml_importation").exists():
+    dir_xml_path = Path("../xml_importation")
+else:
+    raise FileNotFoundError("'../xml_importation' not found.")
+if Path("../files").exists():
+    dir_xml_path = Path("../files")
+else:
+    raise FileNotFoundError("'../files' not found.")
+
+# Files
+xml_path = Path("../files/gll_psc_v32.xml")
+csv_path = Path("../files/gll_psc_v32.csv")
+gmap_path = Path("../files/galattic_coordinates.csv")
