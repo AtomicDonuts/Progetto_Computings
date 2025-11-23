d30c9063dab28778a2b6b427f717239ffea1c8ac
AtomicDonuts <Pascal98@hotmail.it>
2025-11-23 23:25:45 +0100

    map

--- map/map_example.py
+++ map/map_example.py
@@ -1,7 +1,10 @@
+'''
+wip 
+'''
+import argparse
 import sys
 
 from loguru import logger
-import numpy as np
 import plotly.express as px
 import pandas as pd
 
@@ -13,8 +16,21 @@ def fig_generator(
         input_dataframe=None,
         html_output=custom_paths.htmlmap_path
 ):
-    '''
-    '''
+    """
+    fig_generator 
+    Genera una mappa interattiva delle sorgenti.
+    WIP
+
+    Args:
+        catalog_path (str,pathlib.Path, optional): _description_. Defaults to custom_paths.csv_path.
+
+        input_dataframe (pandas.DataFrame, optional): _description_. Defaults to None.
+
+        html_output (str,pathlib.Path, optional): _description_. Defaults to custom_paths.htmlmap_path.
+
+    Returns:
+        ploty.express.fig: Canvas della mappa interattiva.
+    """
     if input_dataframe is None:
         logger.info(f"Loading from {catalog_path}")
         dataframe = pd.read_csv(catalog_path)
@@ -54,5 +70,32 @@ def fig_generator(
         margin={"r": 0, "t": 50, "l": 0, "b": 0},
         dragmode=False,
     )
+
     fig.write_html(html_output)
+    logger.info(f"Mappa generata con successo! Aperta in: {html_output}")
     return fig
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Genera la mappa in hmtl nel file di output."
+    )
+    parser.add_argument(
+        "--input_path",
+        "-i",
+        default=f"{custom_paths.csv_path}",
+        help="Path del cataologo in csv.",
+    )
+    parser.add_argument(
+        "--html_path",
+        "-o",
+        default=f"{custom_paths.htmlmap_path}",
+        help="Path di output del file html.",
+    )
+    args = parser.parse_args()
+
+    fig_generator(
+        catalog_path=custom_paths.csv_path,
+        html_output=custom_paths.htmlmap_path
+    )
+    
