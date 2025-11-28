8342d2eec01b2854e191f79f03eb25553f10a607
AtomicDonuts <Pascal98@hotmail.it>
2025-11-28 19:15:24 +0100

    sto pushando sul main, peccato mortale

--- imports/custom_variables.py
+++ imports/custom_variables.py
@@ -13,18 +13,31 @@ if Path("../imports").exists():
     dir_imports_path = Path("../imports")
 else:
     raise FileNotFoundError("'../imports' not found.")
+
 if Path("../map").exists():
     dir_map_path = Path("../map")
 else:
     raise FileNotFoundError("'../map' not found.")
+
 if Path("../xml_import").exists():
-    dir_xml_path = Path("../xml_importation")
+    dir_xml_path = Path("../xml_import")
 else:
-    raise FileNotFoundError("'../xml_importation' not found.")
+    raise FileNotFoundError("'../xml_import' not found.")
+
 if Path("../files").exists():
     dir_files_path = Path("../files")
 else:
     raise FileNotFoundError("'../files' not found.")
+
+if Path("../fits_import").exists():
+    dir_files_path = Path("../fits_import")
+else:
+    raise FileNotFoundError("'../fits_import' not found.")
+
+if Path("../ann").exists():
+    dir_files_path = Path("../ann")
+else:
+    raise FileNotFoundError("'../ann' not found.")
 
 # Files
 xml_path = Path("../files/gll_psc_v32.xml")
@@ -32,4 +45,4 @@ fits_path = Path("../files/gll_psc_v35.fit")
 csv_path = Path("../files/gll_psc.csv")
 gmap_path = Path("../files/galattic_coordinates.csv")
 map_path = Path("../map/index.html")
-#docs_path = Path("../docs/index.html")
+# docs_path = Path("../docs/index.html")
