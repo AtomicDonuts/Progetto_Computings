f68d89d9f956df4876232fec087fdd43e78f998c
AtomicDonuts <Pascal98@hotmail.it>
2025-12-13 23:47:59 +0100

    GitHub Action Sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,51 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../imports"))
+sys.path.insert(0, os.path.abspath("../../dnn"))
+sys.path.insert(0, os.path.abspath("../../map"))
+sys.path.insert(0, os.path.abspath("../../fits_import"))
+
+project = 'DNN-4FGL'
+copyright = '2025, Pasquale Napoli'
+author = 'Pasquale Napoli'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",  # Legge le docstring dal codice
+    "sphinx.ext.viewcode",  # Aggiunge link al codice sorgente
+    "sphinx.ext.napoleon",  # Supporta stile Google/NumPy (opzionale, ma utile)
+    "sphinx.ext.mathjax",  # Per formule matematiche se ne usi
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
+
+autodoc_mock_imports = [
+    "tensorflow",
+    "keras",
+    "numpy",
+    "pandas",
+    "matplotlib",
+    "plotly",
+    "astropy",
+    "sklearn",
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+.. DNN-4FGL documentation master file, created by
+   sphinx-quickstart on Sat Dec 13 22:53:15 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+DNN-4FGL documentation
+======================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api/dnn/modules
+   api/fits_import/modules
+   api/map/modules
+   api/imports/modules
